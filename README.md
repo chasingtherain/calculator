29e6b0246130e529d65fb8baa0dd95448fdf8724
ChasingRain <junpeng94@gmail.com>
2022-04-22 17:21:35 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # calculator
 a deceptively "easy" app that we use daily, created with vanilla JS
+
+#pseudocode
+
+html structure
+div 1: display screen
+div 2: 2 unit sized clear and delete
+div 3-6: buttons
+
+special operator behavior
+- if user selects operator without typing any number, default current number is 0
+==> small display will be: "0 x"...
+==> if user selects another operator, small display will update, big display will remain as blank
+==> if user selects the same operator, no change
+
+
+after selecting currentnumber and an operator: 
+==> second number selected will be shown as current number in big display
+==> first number will now be prev number and second number will be current number
+
+at this point, if another operator is pressed, calculator will evaluate the operation between prev and current number
+
+expected result: 
+- both small and big display will show evaluated output
+- small display will display the second operator
+
+first no. pressed: update big display
+==> updateBigDisplay()
+
+first oper pressed: update small display with first no. and first oper
+
+second no. pressed: update big display with second no.
+second oper pressed: update small display with eval figure and second oper
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,197 @@
+//main buttons
+const numberBtn = document.querySelectorAll("[data-number]");
+const operatorBtn = document.querySelectorAll("[data-operator]")
+const equalBtn = document.getElementById("equal")
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
+const dotBtn = document.getElementById("dot")
+
+let operatorSelected = false;
+let prevNumber = 0;
+let currentNumber = "";
+let operatorCount = 0;
+let dotCount = 0; // prevent user from typing consecutive decimal places
+
+// main interfaces
+let bigDisplay = document.getElementById("big-display")
+let smallDisplay = document.getElementById("small-display")
+let keyPress;
+let operator;
+
+// let keyPress;
+
+// math operations
+function add(a,b){
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function operate(operator,a,b){
+    console.log("function triggered")
+    let result = null;
+    switch(operator)
+    {
+        case "+":
+            result = add(a,b)
+            break;
+        case "-":
+            result = subtract(a,b)
+            break;
+        case "×":
+            result = multiply(a,b)
+            break;
+        case "÷":
+            result = divide(a,b)
+            break;
+        default:
+            result = "no operator chosen";
+            break;
+    }
+    return result;
+}
+
+// event listeners
+
+//   add event listener for number buttons
+numberBtn.forEach(
+    button => button.addEventListener("click", (button) => {
+        keyPress = button.target.textContent;
+        updateBigDisplay(keyPress)
+        // console.log(keyPress)
+        console.log("prev number: " + prevNumber)
+        console.log("curr number: " + currentNumber)
+    })
+)
+
+//   add event listener for operator button
+operatorBtn.forEach(
+    button => button.addEventListener("click", (button)=>{
+        keyPress = button.target.textContent;
+        operandUpdate(keyPress)
+    })
+    )
+    
+    //add event listener for equal button
+    equalBtn.addEventListener("click", () => {
+        evaluateOutput();
+    })
+    
+    // add event listener for dot button
+    dotBtn.addEventListener("click",(button) =>{
+        keyPress = button.target.textContent;
+        dotCount+=1;
+        if(!multiDecimal()){
+            updateBigDisplay(keyPress)
+        }
+    })
+    
+
+    // add event listener for keydown
+    window.addEventListener("keydown", (event)=>{
+            key = event.key;
+            if (key/1 == key){
+                console.log(key)
+            }
+        })
+    
+    
+
+function operandUpdate(keyPress){
+    // console.log(keyPress)
+    operatorSelected = true;
+    operatorCount+=1;
+    if(operatorCount <= 1){
+        prevNumber = currentNumber;
+        // currentNumber = ""
+    }
+    if(operatorCount >= 2){
+        evaluateOutput()
+    }
+    operator = keyPress
+    updateSmallDisplay(prevNumber,operator)
+    dotCount = 0;
+    console.log("prev number: " + prevNumber)
+    console.log("curr number: " + currentNumber)
+    console.log("oper count: " + operatorCount)
+}
+
+
+//update small display
+function updateSmallDisplay(prevNumber,operator){
+    smallDisplay.textContent = prevNumber + " " + operator;
+}
+
+//update big display
+function updateBigDisplay(keyPress){
+    if (operatorSelected){
+        currentNumber = "";
+        operatorSelected = false;
+        // console.log("operator exist!")
+    }
+    currentNumber += keyPress
+    bigDisplay.textContent = currentNumber;
+}
+
+function clearBigDisplay(){
+    bigDisplay.textContent = "";
+}
+
+// delete last value event listener
+deleteBtn.addEventListener("click",deleteNum)
+function deleteNum(){
+    currentNumber = bigDisplay.textContent.slice(0,-1);
+    bigDisplay.textContent = currentNumber
+    console.log(currentNumber)
+    if (currentNumber.length == 0){ 
+        currentNumber = ""
+    }
+}
+
+//  clear screen event listener
+clearBtn.addEventListener("click", clearScreen)
+function clearScreen(){
+    prevNumber = 0;
+    currentNumber = "";
+    bigDisplay.textContent = "0";
+    smallDisplay.textContent = "";
+    operatorCount=0;
+}
+
+function evaluateOutput(){
+    if(prevNumber != currentNumber){
+        output = operate(operator, prevNumber, currentNumber)
+        let fixedOutput;
+        if (output % 1 != 0){
+            fixedOutput = output.toFixed(2)
+        }
+        else{
+            fixedOutput = output;
+        }
+        bigDisplay.textContent = fixedOutput
+        currentNumber = fixedOutput;
+        prevNumber = fixedOutput;
+        updateSmallDisplay(prevNumber,operator)
+        console.log(currentNumber)
+        console.log(fixedOutput)
+    }
+}
+
+
+// prevent >1 decimal places
+function multiDecimal(){
+    if (dotCount>1){
+        return true;
+    }
+    return false;
+}
